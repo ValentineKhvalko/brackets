5452a54cccaaf9d3bdd8a6831d93f35addb8574f
Valentine <esfeeder@gmail.com>
2020-03-15 13:25:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const result = [];
+
+  str.split('').forEach((symbol) => {
+    const currentBracketPair = bracketsConfig.find((pair) => {
+      return pair.some((pairBracket) => pairBracket === symbol);
+    });
+
+    if (symbol === currentBracketPair[1]) {
+      if (currentBracketPair[0] === result[result.length-1]) {
+        result.pop();
+      } else {
+        result.push(symbol);
+      }
+    } else {
+      result.push(symbol);
+    }
+  });
+
+  return !result.length;
+};
+  
